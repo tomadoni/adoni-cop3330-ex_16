f30072d8a85ae3c8ff9a41bd3359e8987a02344a
Tom Adoni <tomad101@gmail.com>
2021-06-07 18:19:21 -0400

    Initial commit

---
+++ src/exercise16.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+	    Scanner kb= new Scanner(System.in);
+	    int legalAge=16;
+	    int age=0;
+	    System.out.println("What is your age?");
+	    age= kb.nextInt();
+	    if(age<legalAge){
+	        System.out.println("You are not old enough to legally drive.");
+	    }
+	    else{
+	        System.out.println("You are old enough to legally drive.");
+        }
+    }
+}
